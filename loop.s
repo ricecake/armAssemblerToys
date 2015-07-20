f7641405ecb43261a418cca25758b3f38cc10394
Sebastian Green-Husted <ricecake@tfm.nu>
2015-07-19 22:44:11 -0400

    Create loop.s

---
+++ loop.s
@@ -0,0 +1,12 @@
+.global main /* 'main' is our entry point and must be global */
+.func main   /* 'main' is a function */
+ 
+main:          /* This is main */
+    mov r0, #0 /* Put a 0 inside the register r0 */
+.loop:
+    cmp r0, #16
+    beq .done
+    add r0, #1
+    b .loop
+.done:
+    bx lr      /* Return from main */
